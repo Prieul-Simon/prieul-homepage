9dbe56e1ed8c5f2ebc6909813db938b50a47ef31
Prieul-Simon <simon.prieul@gmail.com>
2025-01-30 21:28:05 +0100

    First commit

---
+++ src/content_en.js
@@ -0,0 +1,3 @@
+import { html } from './en.md'
+
+document.getElementsByTagName('body')[0].innerHTML = html
---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { Plugin } from 'vite'
+import { defineConfig } from 'vite'
+import mdPlugin, { Mode } from 'vite-plugin-markdown'
+
+const linkTargetBlankPatch = (): Plugin => ({
+    name: 'link-target-blank-patch',
+    enforce: 'pre',
+    transform(code) {
+        return code.replaceAll('<a href=', '<a target=\\"_blank\\" href=')
+    },
+})
+
+export default defineConfig({
+    plugins: [
+        mdPlugin({
+        mode: [Mode.HTML],
+    }),
+    linkTargetBlankPatch(),
+],
+})
